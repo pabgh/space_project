7720fd488b3ed350724778706125fd531ce68377
Pedro Alves <pedroaborges@outlook.com.br>
2023-07-04 20:12:56 -0300

    dicionario de dados

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request, 'gallery/index.html')
+
+    dados = {
+    1: {"nome": "Nebulosa de Carina",
+        "legenda": "webtelescope.org / NASA / James Webb Space Telescope"},
+    2: {"nome": "Nebulosa de Orion",
+        "legenda": "nasa.org / NASA / Hubble Space Telescope"},
+    }
+
+    return render(request, 'gallery/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'gallery/imagem.html')
